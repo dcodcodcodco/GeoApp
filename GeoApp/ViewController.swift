ebf27bc8fd7ec70b153840a5707efdbc7022b7c1
dcodocdcodco <snoozelight0@gmail.com>
2021-11-16 23:13:56 +0300

    Initial Commit

---
+++ GeoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeoApp
+//
+//  Created by Alex on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
